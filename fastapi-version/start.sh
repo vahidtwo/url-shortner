36e7a10faf188c9606f95d6abfce17266b35a641
vahidtwo <vahidtwo@gmail.com>
2024-10-25 17:44:17 +0330

    :bug: fix app import after move if to another file

--- fastapi-version/start.sh
+++ fastapi-version/start.sh
@@ -2,7 +2,7 @@
 echo $ENTRYPOINT
 if [[ "$ENTRYPOINT" = "web" ]]
 then
-   echo "Starting web" && gunicorn --config gunicorn_config.py  app.main:app
+   echo "Starting web" && gunicorn --config gunicorn_config.py  url_shortener.app:fastapi_appf
 else
   echo Error, cannot find entrypoint "$ENTRYPOINT" to start
 fi
